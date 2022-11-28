053a6a45be24313c0c5b98d3c6f25686f6e1f810
ROCK05 <arjunmajithiya2332@gmail.com>
2022-11-29 04:30:45 +0530

    Added Level3

---
+++ decodeLevel3.h
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+#include "decodeLevel2.h"
+#include "autoCorrectPackage\autoCorrectMain.h"
+
+using namespace std;
+void level3(ifstream &decodedFileLevel2) 
+{
+    //Level 3
+    while(!decodedFileLevel2.eof())
+    {
+        ofstream finalFile("Files/OutputLevel3.txt",ios::out | ios::trunc);
+        string word = "";
+        char c;
+        decodedFileLevel2.get(c);
+        if(decodedFileLevel2.eof()) break;
+
+        if(c != ' ' and punMap.find(c) == punMap.end())
+        {
+            word.push_back(c);
+        }
+        else
+        {
+            if(word.size())
+            {
+                word = spellingCorrector(word);
+                finalFile << word;
+                word = "";
+            }
+            finalFile << c;
+        }
+    }
+}
